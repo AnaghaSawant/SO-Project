e4c568ad8ee7bfb749c43c62dcaf032551f124dd
AnaghaSawant <anagha@treeni.com>
2019-08-01 21:53:16 +0530

    feature::Sustain-online: add code for accessing sustainability goal course and its sub course

--- loginpage.py
+++ loginpage.py
@@ -1,16 +1,30 @@
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 import time
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.common.action_chains import ActionChains
+from selenium.webdriver.support import expected_conditions as EC
 driver = webdriver.Chrome()
+from selenium.webdriver.common.by import By
+
+# class SustainOnline
 driver.get("https://appstage.sustainonline.com/")
-time.sleep(10)
+time.sleep(5)
 driver.find_element_by_link_text('Close').click()
 username = driver.find_element_by_name("login")
 password = driver.find_element_by_name("password") 
 username.send_keys("gaurav+z11@treeni.com")
 password.clear()
 password.send_keys("Treeni@#1232")
-# driver.implicitly_wait(10)
-a = driver.find_element_by_xpath("//button[@class='so-btn so-btn-primary']")
-# a = driver.find_element_by_class_name('so-btn-primary')
-a.click()
+
+login = driver.find_element_by_xpath("//button[@class='so-btn so-btn-primary']")
+login.click()
+time.sleep(3)
+sustain_dev_goals = driver.find_elements_by_class_name('so-course-card-title')[2]
+sustain_dev_goals.click()
+time.sleep(3)
+sub_course = driver.find_elements_by_class_name('so-course-card-small')[1]
+sub_course.click()
+time.sleep(3)
+start_course = driver.find_element_by_xpath("//div[@class='overview-sidebar__header-wrap brand--head']")
+start_course.click()
